c496ff271d20850da2c97fc708ada4c5f0648a97
tiemonl <tiemon.liam@gmail.com>
2019-09-02 14:19:47 -0400

    changed class name for unload all coupons

--- unloadCouponsFromCard.js
+++ unloadCouponsFromCard.js
@@ -1,5 +1,5 @@
 var maxCouponsAllowed = 150;
-var unloadCouponFromCardClassName = "Button Button--red Button--small Color";
+var unloadCouponFromCardClassName = "kds-Button kds-Button--destructiveSecondary kds-Button--compact CouponButton";
 for (var i = 0; i < maxCouponsAllowed; i++) {
 	document.getElementsByClassName(unloadCouponFromCardClassName)[i].click();
 	setTimeout(function(){}, 250); //Time out to not overload website.
