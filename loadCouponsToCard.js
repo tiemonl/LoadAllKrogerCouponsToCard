0b1c2e5421b9a5f0fd6b6026f63265f41c7ebe54
tiemonl <tiemon.liam@gmail.com>
2019-09-02 14:19:13 -0400

    changed class name for add

--- loadCouponsToCard.js
+++ loadCouponsToCard.js
@@ -1,5 +1,5 @@
 var maxCouponsAllowed = 150;
-var loadCouponToCardClassName = "Button Button--green Button--small Color";
+var loadCouponToCardClassName = "kds-Button kds-Button--favorable kds-Button--compact CouponButton";
 for (var i = 0; i < maxCouponsAllowed; i++) {
 	document.getElementsByClassName(loadCouponToCardClassName)[i].click();
 	setTimeout(function(){}, 250); //Time out to not overload website.
